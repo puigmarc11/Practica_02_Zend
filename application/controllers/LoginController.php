eac2587ee52c0acacebc474e737ad3e6637867a1
Usuari <Usuari@P125>
2017-02-03 19:27:24 +0100

    f

--- application/controllers/LoginController.php
+++ application/controllers/LoginController.php
@@ -7,21 +7,34 @@ class LoginController extends Zend_Controller_Action {
     }
 
     public function indexAction() {
-        
+
         if ($this->getRequest()->isPost()) {
-          
+
             $dadesLogin = $this->getRequest()->getPost();
-            
+
             $nom = $dadesLogin["nom"];
             $pass = $dadesLogin["password"];
-            
-            if($nom == "admin" && $pass == "admin"){
+
+            if ($nom == "admin" && $pass == "admin") {
                 $this->redirect("Administrador/index");
+                exit();
             }
-        } 
-       
-        
-        
+
+            $alumnes = new Application_Model_DbTable_Alumnes();
+            $d = $alumnes->find($nom);
+
+
+            $row = $alumnes->fetchRow(
+                    $alumnes->select()
+                            ->where('dni = ?', $nom)
+                            ->where('password = ?', $pass)
+            );
+
+            if ($row) {
+                $this->redirect("Festa/index");
+                exit();
+            }
+        }
     }
 
 }
